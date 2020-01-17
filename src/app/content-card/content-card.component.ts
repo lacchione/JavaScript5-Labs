a29000e7b7c6b8425dbea7dba124c40a820fcc10
Luca Acchione <lucaacchione@Lucas-MacBook-Air.local>
2020-01-17 12:57:57 -0500

    Finished Content Card creation, along with pipe creation

--- src/app/content-card/content-card.component.ts
+++ src/app/content-card/content-card.component.ts
@@ -1,5 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Content {
+    readonly id: number;
+    author: string;
+    imgUrl?: string;
+    type?: string;
+    title: string;
+    body: string;
+    tags?: string[];
+}
+
+class ContentList {
+    private _types: Content[];
+    constructor() {
+        this._types = [];
+    }
+    get types(): Content[] {
+        return this._types;
+    }
+    set types(i: Content[]) {
+        this._types = i;
+    }
+    addContent(content: Content){
+      this._types.push(content);
+    }
+    countContent() {
+        return this._types.length;
+    }
+    contentHtml(i:number){
+
+    }
+}
+
 @Component({
   selector: 'app-content-card',
   templateUrl: './content-card.component.html',
@@ -7,7 +39,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContentCardComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+  }
 
   ngOnInit() {
   }

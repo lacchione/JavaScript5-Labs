3b60f78621a37896cdc308fbd1063ec8c1e9333c
Luca Acchione <lucaacchione@Lucas-MacBook-Air.local>
2020-01-31 11:12:06 -0500

    Initial Commit for Assignment 3

--- src/app/content-list/content-list.component.ts
+++ src/app/content-list/content-list.component.ts
@@ -16,7 +16,7 @@ export class ContentListComponent implements OnInit {
             id: 1,
             author: 'Luca',
             imgUrl: 'http://www.inspiremykids.com/wp-content/uploads/2017/02/gamecontroler2.jpeg',
-            type: 'Video Games',
+            type: 'Story',
             title: 'How to play Video Games',
             body: 'This is how you play Video Games',
             tags: ['Video Games', ' Luca']
@@ -25,7 +25,7 @@ export class ContentListComponent implements OnInit {
             id: 2,
             author: 'Will',
             imgUrl: 'https://cdn.iconscout.com/icon/premium/png-256-thumb/book-shop-2-1104657.png',
-            type: 'Books',
+            type: 'Story',
             title: 'How to run a Bookstore',
             body: 'This is how you start a bookstore',
             tags: ['Books', ' Will']
@@ -34,7 +34,7 @@ export class ContentListComponent implements OnInit {
             id: 3,
             author: 'Jack',
             imgUrl: 'http://icons.iconarchive.com/icons/thehoth/seo/256/seo-web-code-icon.png',
-            type: 'Code',
+            type: 'Story',
             title: 'How to write super long code',
             body: 'This is how you write super long code',
             tags: ['Code', ' Jack']
@@ -43,7 +43,7 @@ export class ContentListComponent implements OnInit {
             id: 4,
             author: 'Jaidee',
             imgUrl: 'https://pmforthemasses.com/wp-content/uploads/2014/08/camera.png',
-            type: 'Angular',
+            type: 'News',
             title: 'How to play Angular for Dummies',
             body: 'This is how you Angular',
             tags: ['Angular', ' Jaidee']
@@ -52,7 +52,7 @@ export class ContentListComponent implements OnInit {
               id: 5,
               author: 'Draven',
               imgUrl: 'https://pmforthemasses.com/wp-content/uploads/2014/08/camera.png',
-              type: 'Sky',
+              type: 'News',
               title: 'Sky is Blue',
               body: 'Why the sky is BLUE',
               tags: []}
